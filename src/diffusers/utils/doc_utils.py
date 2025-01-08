783c4f7e1e1272d19544cfd5204c43e9405f5e04
chuson <fujirock2002@gmail.com>
2025-01-08 15:41:07 +0900

    Add doc_utils.py

--- src/diffusers/utils/doc_utils.py
+++ src/diffusers/utils/doc_utils.py
@@ -1,7 +1,11 @@
-# Copyright 2024 The HuggingFace Team. All rights reserved.
+# --------------------------------------------------------------------------------------
+# doc_utils.py (修正版) 
+#  - Adds a check so that if fn.__doc__ is None, we skip splitting it and avoid AttributeError.
+# --------------------------------------------------------------------------------------
+# Copyright 2024 The HuggingFace Team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
+# You may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,6 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """
 Doc utilities: Utilities related to documentation
 """
@@ -21,6 +26,13 @@ import re
 def replace_example_docstring(example_docstring):
     def docstring_decorator(fn):
         func_doc = fn.__doc__
+
+        # [修正] docstring が None の場合、何もしないで返す
+        if func_doc is None:
+            # 何らかの警告を出してもいいかもしれません
+            # ここでは単に docstring 処理をスキップ
+            return fn
+
         lines = func_doc.split("\n")
         i = 0
         while i < len(lines) and re.search(r"^\s*Examples?:\s*$", lines[i]) is None:
